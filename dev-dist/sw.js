26bd45fd5023511f3f7eabdd3c19af7987bd2484
Monat-E <emonat2000@hotmail.com>
2025-06-01 23:06:14 -0400

    correction du fichier .env

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-34b6f136'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.orkgdj36n9"
+    "revision": "0.b4v069r4bn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
